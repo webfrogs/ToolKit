f5f6df57ebc44af1f4cca0cc6ee7cde65f10b686
Carl Chen <ccf.developer@gmail.com>
2018-03-29 17:08:44 +0800

    Change some setting in vs code

--- configs/vscode/vscode-configer.sh
+++ configs/vscode/vscode-configer.sh
@@ -14,5 +14,15 @@ if [[ -L "${VSCodeSettingFilePath}" ]]; then
 	rm "${VSCodeSettingFilePath}"
 fi
 ln -s "${ShellFolderPath}/settings.json" "${VSCodeSettingFilePath}"
+
+VSCodeKeybindingFilePath="${HOME}/Library/Application Support/Code/User/keybindings.json"
+if [[ -f "${VSCodeKeybindingFilePath}" ]]; then
+	rm "${VSCodeKeybindingFilePath}"
+fi
+if [[ -L "${VSCodeKeybindingFilePath}" ]]; then
+	rm "${VSCodeKeybindingFilePath}"
+fi
+ln -s "${ShellFolderPath}/keybindings.json" "${VSCodeKeybindingFilePath}"
+
 echo "VS Code configuration end.\n"
 
