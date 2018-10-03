7abb50f26edf945e7a26d4f1aa37577d6f9206fe
Carl Chen <ccf.developer@gmail.com>
2018-10-03 08:48:41 +0800

    Support macOS install for fzf installation script

--- installer/fzf/install.sh
+++ installer/fzf/install.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 set -e
 
-InstallationPath=$HOME/.fzf
-if [ -d "${InstallationPath}" ]; then
-    echo "Fzf is already installed."
-    exit 0
-fi
+case "$(uname -s)" in
+    Darwin)
+        brew install fzf
+        $(brew --prefix)/opt/fzf/install --all
+        ;;
+    Linux)
+        InstallationPath=$HOME/.fzf
+        if [ -d "${InstallationPath}" ]; then
+            echo "Fzf is already installed."
+            exit 0
+        fi
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ${InstallationPath}
-${InstallationPath}/install --all
+        git clone --depth 1 https://github.com/junegunn/fzf.git ${InstallationPath}
+        ${InstallationPath}/install --all
+        ;;
+    *)
+        echo "Unsupported OS is found."
+        exit 1
+        ;;
+esac
+
