be491208891f6e4d302d27aedb864eda79b6bbf3
Carl Chen <ccf.developer@gmail.com>
2019-04-02 18:51:09 +0800

    Add China mirror options to docker installer

--- installer/docker/install.sh
+++ installer/docker/install.sh
@@ -12,6 +12,11 @@ case "$(uname -s)" in
         exit
         ;;
     Linux)
+        read -p "Need China mirror? [y/n]: " chinaMirror
+        if [ ! "${chinaMirror}" == "y" ]; then
+            chinaMirror=""
+        fi
+
         if test -x "$(command -v apt-get)"; then
             sudo apt-get update
             sudo apt-get install -y \
@@ -21,17 +26,34 @@ case "$(uname -s)" in
                 gnupg2 \
                 software-properties-common
             curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-            sudo add-apt-repository "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu bionic stable"
+            if [ -z "${chinaMirror}" ]; then
+                sudo add-apt-repository \
+                    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+                    $(lsb_release -cs) \
+                    stable"
+            else
+                sudo add-apt-repository \
+                    "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu \
+                    bionic stable"
+            fi
             sudo apt-get update
             sudo apt-get install -y docker-ce
+            sudo systemctl start docker
         elif test -x "$(command -v yum)"; then
             sudo yum install -y yum-utils \
                 device-mapper-persistent-data \
                 lvm2
-            sudo wget -O /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/centos/docker-ce.repo
-            sudo sed -i 's+download.docker.com+mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
-            sudo yum makecache fast
-            sudo yum install -y docker-ce
+            if [ -z "${chinaMirror}" ]; then 
+                sudo yum-config-manager \
+                    --add-repo \
+                    https://download.docker.com/linux/centos/docker-ce.repo
+            else
+                sudo wget -O /etc/yum.repos.d/docker-ce.repo \
+                    https://download.docker.com/linux/centos/docker-ce.repo
+                sudo sed -i 's+download.docker.com+mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
+                sudo yum makecache fast
+            fi
+            sudo yum install -y docker-ce docker-ce-cli containerd.io
             sudo systemctl start docker
         else
             echo "[Error] Can not find support package management."
