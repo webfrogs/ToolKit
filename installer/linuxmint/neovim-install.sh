13ad4063211155950ddecf0aae221e28efe4b855
Carl Chen <ccf.developer@gmail.com>
2019-11-16 21:25:15 +0800

    feat: install nodejs if not exists when install neovim

--- installer/linuxmint/neovim-install.sh
+++ installer/linuxmint/neovim-install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
+if ! test -x "$(command -v node)"; then
+	echo "deb https://deb.nodesource.com/node_12.x bionic main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+	echo "deb-src https://deb.nodesource.com/node_12.x bionic main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+	sudo apt-get update -y
+	sudo apt-get install -y nodejs
+fi
+
+sudo add-apt-repository -y ppa:neovim-ppa/stable
+sudo apt-get update -y
+sudo apt-get install -y neovim
