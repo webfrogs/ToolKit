9423287ac478989c13e12e12087687932bf629fd
Carl Chen <ccf.developer@gmail.com>
2019-11-29 22:36:32 +0800

    fix: add missing public key for nodejs apt repo in nvim installation script

--- installer/linuxmint/neovim-install.sh
+++ installer/linuxmint/neovim-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+set -e
 
 if ! test -x "$(command -v node)"; then
+	curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
 	echo "deb https://deb.nodesource.com/node_12.x bionic main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 	echo "deb-src https://deb.nodesource.com/node_12.x bionic main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 	sudo apt-get update -y
