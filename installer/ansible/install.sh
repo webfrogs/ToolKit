fcb67697cfe6f1e4365bbcbdbf2a64c2d129ee65
Carl Chen <ccf.developer@gmail.com>
2020-06-20 14:56:07 +0800

    fix: ansible installation script bug

--- installer/ansible/install.sh
+++ installer/ansible/install.sh
@@ -6,8 +6,8 @@ case "$(uname -s)" in
 	Linux)
 		if test -x "$(command -v apt-get)"; then
 			sudo apt update
-			sudo apt install software-properties-common
-			sudo apt-add-repository -y update ppa:ansible/ansible
+			sudo apt install -y software-properties-common
+     sudo apt-add-repository -y ppa:ansible/ansible
 			sudo apt update
 			sudo apt install -y ansible
 		else
