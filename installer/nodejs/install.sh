6549f725ee3c02eedc78ae68e918df383ec3f367
Carl Chen <ccf.developer@gmail.com>
2020-10-07 19:51:28 +0800

    fix: node installation script bug

--- installer/nodejs/install.sh
+++ installer/nodejs/install.sh
@@ -6,6 +6,8 @@ if test -x "$(command -v node)"; then
 	exit
 fi
 
+mkdir -p $HOME/.nvm
+
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh -o /tmp/install_nvm.sh
 bash /tmp/install_nvm.sh
 export NVM_DIR="$HOME/.nvm"
