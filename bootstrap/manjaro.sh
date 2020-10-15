418e7863dde367c206507b2d21364cf22af2cbf7
Carl Chen <ccf.developer@gmail.com>
2020-10-15 12:46:28 +0800

    feat: add manjaro bootstrap script

--- bootstrap/manjaro.sh
+++ bootstrap/manjaro.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
 
-ShellFolderPath=$( cd "$( dirname "$0" )" && pwd )
-cd "${ShellFolderPath}"
-ConfigFolderPath="${ShellFolderPath}/configs"
+cd $(dirname $0)
+cd ..
 
 if test "$(uname -s)" != "Linux"; then
   echo "[ERROR] Current OS is not Linux"
@@ -15,52 +14,24 @@ if test ! -x "$(command -v pacman)"; then
   exit 2
 fi
 
-exit 0
-
-case "$(uname -s)" in
-	Linux)
-		if test -x "$(command -v apt-get)"; then
-			sudo apt-get update
-			sudo apt-get -y install \
-				git build-essential automake autoconf resolvconf \
-				pkg-config vim cmake python python-dev zsh
-		elif test -x "$(command -v yum)"; then
-			sudo yum update
-			sudo yum install -y \
-				git vim zsh
-		elif test -x "$(command -v pacman)"; then
-			sudo pacman -Syy
-			sudo pacman-mirrors -i -c China -m rank
-			if grep -Fxq "[archlinuxcn]" /etc/pacman.conf; then
-				echo "[INFO] archlinux cn already exists in file '/etc/pacman.conf'"
-			else
-				echo "[archlinuxcn]" | sudo tee -a /etc/pacman.conf
-				echo "SigLevel = Optional TrustedOnly" | sudo tee -a /etc/pacman.conf
-				echo 'Server = http://mirrors.tuna.tsinghua.edu.cn/archlinuxcn//$arch' | sudo tee -a /etc/pacman.conf
-				sudo pacman -Syy
-				sudo pacman -S archlinuxcn-keyring
-			fi
-
-			sudo pacman -Syy git vim zsh unzip
-		else
-			echo "[Error] No package management tools found."
-			exit 2
-		fi
-
-		if [ ! -d "$HOME/.fzf" ]; then
-			git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
-			$HOME/.fzf/install --all
-		fi
-
-		;;
-	*)
-		echo "[Error] Unsupported OS"
-		exit 2
-		;;
-esac
+sudo pacman -Syy
+sudo pacman-mirrors -i -c China -m rank
+if grep -Fxq "[archlinuxcn]" /etc/pacman.conf; then
+	echo "[INFO] archlinux cn already exists in file '/etc/pacman.conf'"
+else
+	echo "[archlinuxcn]" | sudo tee -a /etc/pacman.conf
+	echo "SigLevel = Optional TrustedOnly" | sudo tee -a /etc/pacman.conf
+	echo 'Server = http://mirrors.tuna.tsinghua.edu.cn/archlinuxcn//$arch' | sudo tee -a /etc/pacman.conf
+	sudo pacman -Syy
+	sudo pacman -S archlinuxcn-keyring
+fi
+sudo pacman -Syy git vim zsh unzip
 
 ./configs/git/git-configer.sh
 git submodule update --init --recursive
 ./configs/zsh/zsh-configer.sh
 
-echo "[Info] All finished."
+./installer/docker/install.sh
+./installer/nodejs/install.sh
+./installer/neovim/install.sh
+
