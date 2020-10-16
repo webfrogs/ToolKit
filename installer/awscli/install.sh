fe7905ae35f76f2821dd6a50882f3fe0dc15a3cd
Carl Chen <ccf.developer@gmail.com>
2020-10-16 12:57:07 +0800

    feat: install aws version 2 instead of version 1 in aws cli installation script

--- installer/awscli/install.sh
+++ installer/awscli/install.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 set -e
 
-pip3 install wheel
-pip3 install awscli --upgrade --user
+case "$(uname -s)" in
+	Darwin)
+		;;
+	Linux)
+    cd /tmp
+    rm -rf aws
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+		;;
+	*)
+		echo "[ERROR] Unsupported OS"
+		exit 2
+		;;
+esac
+
+
