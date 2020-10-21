0969d506a5086e5e9cc3078a224a6a27692f037a
Carl Chen <ccf.developer@gmail.com>
2020-10-21 09:18:10 +0800

    feat: add zsh autosuggestions plugin

--- configs/zsh/config.sh
+++ configs/zsh/config.sh
@@ -10,6 +10,7 @@ ShellFolderPath=$(cd $(dirname $0) && pwd)
 cd "${ShellFolderPath}"
 
 OhMyZshPath="$HOME/.config/oh-my-zsh"
+OhMyZshCustomPath="$OhMyZshPath/custom"
 mkdir -p $(dirname $OhMyZshPath)
 
 # Install or update oh-my-zsh
@@ -19,21 +20,30 @@ if [ ! -d "$OhMyZshPath" ]; then
 else
   echo "[INFO] Updating oh-my-zsh..."
   cd "$OhMyZshPath"
-	git submodule update --init --recursive
+  git fetch origin
+  git reset --hard origin/master
 	cd "${ShellFolderPath}"
 fi
 
 ln -sf "${ShellFolderPath}/_zshrc" "$HOME/.zshrc"
 
-ZshCustomCompletionFolderPath=$OhMyZshPath/custom/plugins/carl
-if [[ -d "${ZshCustomCompletionFolderPath}" ]]; then
-	rm -rf "${ZshCustomCompletionFolderPath}"
+ZshCustomCompletionFolderPath=$OhMyZshCustomPath/plugins/carl
+if test -d "${ZshCustomCompletionFolderPath}"; then
+  rm -rf "${ZshCustomCompletionFolderPath}"
 fi
-if [[ -L "${ZshCustomCompletionFolderPath}" ]]; then
-	rm "${ZshCustomCompletionFolderPath}"
-fi
-ln -s "${ShellFolderPath}/customCompletions" "${ZshCustomCompletionFolderPath}"
+ln -sf "${ShellFolderPath}/customCompletions" "${ZshCustomCompletionFolderPath}"
 
+#zsh plugins
+# zsh-autosuggestions
+autosuggestionsPluginPath=$OhMyZshCustomPath/plugins/zsh-autosuggestions
+if test -d "$autosuggestionsPluginPath"; then
+  cd $autosuggestionsPluginPath
+  git fetch origin
+  git reset --hard origin/master
+  cd $ShellFolderPath
+else
+  git clone https://github.com/zsh-users/zsh-autosuggestions $autosuggestionsPluginPath
+fi
 
 #ZshCustomThemeFolderPath=$OhMyZshPath/custom/themes
 #BulletTrainThemeFolderPath="${ZshCustomThemeFolderPath}/bullet-train-oh-my-zsh-theme"
@@ -45,13 +55,15 @@ ln -s "${ShellFolderPath}/customCompletions" "${ZshCustomCompletionFolderPath}"
 #fi
 #ln -s "${ShellFolderPath}/customThemes/bullet-train-oh-my-zsh-theme" "${BulletTrainThemeFolderPath}"
 
-echo ""
-read -p "Set zsh as default shell? [y/n]: " shouldSetDefault
-if [ ! "${shouldSetDefault}" == "y" ]; then
-    shouldSetDefault=""
-fi
-if test "${shouldSetDefault}" = "y"; then
-  chsh -s $(which zsh)
+if test $(basename $SHELL) != "zsh"; then
+  echo ""
+  read -p "Zsh is not default shell, set it as default? [y/n]: " shouldSetDefault
+  if [ ! "${shouldSetDefault}" == "y" ]; then
+      shouldSetDefault=""
+  fi
+  if test "${shouldSetDefault}" = "y"; then
+    chsh -s $(which zsh)
+  fi
 fi
 
 echo "[INFO] zsh is installed successfully."
