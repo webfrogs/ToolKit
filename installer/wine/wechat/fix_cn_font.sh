440ccde9079a6226dd46da6ceab2f6dc376dcc6c
Carl Chen <ccf.developer@gmail.com>
2021-06-09 19:25:19 +0800

    fix: wechat cn font script

--- installer/wine/wechat/fix_cn_font.sh
+++ installer/wine/wechat/fix_cn_font.sh
@@ -7,14 +7,14 @@ set -e
 cat << EOF > /tmp/msyh_config.reg
 REGEDIT4
 [HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\FontLink\SystemLink]
-"Lucida Sans Unicode"="msyh.ttc"
-"Microsoft Sans Serif"="msyh.ttc"
-"MS Sans Serif"="msyh.ttc"
-"Tahoma"="msyh.ttc"
-"Tahoma Bold"="msyhbd.ttc"
-"msyh"="msyh.ttc"
-"Arial"="msyh.ttc"
-"Arial Black"="msyh.ttc"
+"Lucida Sans Unicode"="msyh.ttf"
+"Microsoft Sans Serif"="msyh.ttf"
+"MS Sans Serif"="msyh.ttf"
+"Tahoma"="msyh.ttf"
+"Tahoma Bold"="msyhbd.ttf"
+"msyh"="msyh.ttf"
+"Arial"="msyh.ttf"
+"Arial Black"="msyh.ttf"
 EOF
 
 env WINEPREFIX="$HOME/.deepinwine/Deepin-WeChat" deepin-wine5 regedit /tmp/msyh_config.reg
