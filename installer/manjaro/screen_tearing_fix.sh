014fa2eb30b82f1be8f55adb759f6d7fd25adbc4
Carl Chen <ccf.developer@gmail.com>
2021-12-22 17:33:07 +0800

    refactor: add comment for manjaro screen tearing fix script

--- installer/manjaro/screen_tearing_fix.sh
+++ installer/manjaro/screen_tearing_fix.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Run this script to fix screen tearing problem for intel Graphics machine
+# https://wiki.archlinux.org/title/intel_graphics#Tearing
+
 sudo tee -a /etc/X11/xorg.conf.d/20-intel.conf >/dev/null <<'EOF'
 Section "Device"
   Identifier "Intel Graphics"
