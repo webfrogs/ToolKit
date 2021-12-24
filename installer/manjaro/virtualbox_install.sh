632da63e388e8b40a8df6fa5cb28dda741cb201d
Carl Chen <ccf.developer@gmail.com>
2021-12-24 10:00:38 +0800

    fix: manjaro install virtualbox issue

--- installer/manjaro/virtualbox_install.sh
+++ installer/manjaro/virtualbox_install.sh
@@ -2,5 +2,5 @@
 set -e
 
 #linux_kernel=$(mhwd-kernel -li | grep '*' | awk '{print $2}')
-sudo pacman -Syu virtualbox
+sudo pacman -Syu virtualbox ${linux_kernel}-virtualbox-host-modules
 sudo vboxreload
