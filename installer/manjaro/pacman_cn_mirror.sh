11095925c1c68be1cf02161e9ba7fb9e16363fc0
Carl Chen <ccf.developer@gmail.com>
2022-04-19 12:36:41 +0800

    fix: check pacman-mirors command in pacman cn mirror shell script

--- installer/manjaro/pacman_cn_mirror.sh
+++ installer/manjaro/pacman_cn_mirror.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
 
+
+if test ! -x "$(command -v pacman-mirrors)"; then
+  echo "'pacman-mirrors' command is not found"
+  exit
+fi
+
 sudo pacman -Syy
 sudo pacman-mirrors -i -c China -m rank
