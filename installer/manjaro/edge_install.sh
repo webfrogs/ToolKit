0466fd2376a7653059671a32dc9e9f7e8b923f26
Carl Chen <ccf.developer@gmail.com>
2022-09-22 08:55:32 +0800

    fix: manjaro install edge

--- installer/manjaro/edge_install.sh
+++ installer/manjaro/edge_install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 set -e
 
-yay -S microsoft-edge-stable
+# yay -S microsoft-edge-stable
+
+cd /tmp
+git clone https://aur.archlinux.org/microsoft-edge-stable-bin.git
+cd microsoft-edge-stable-bin
+makepkg -si
+cd ..
+rm -rf microsoft-edge-stable-bin
