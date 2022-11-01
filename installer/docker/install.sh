cfd13c57686d3a03fcf0424ca86fbe132c53708c
Carl Chen <ccf.developer@gmail.com>
2022-11-01 17:35:50 +0800

    fix: docker install issue on ubuntu

--- installer/docker/install.sh
+++ installer/docker/install.sh
@@ -37,6 +37,7 @@ case "$(uname -s)" in
             fi
 
             if test "${chinaMirror}" = "y"; then
+              sudo mkdir -p /etc/apt/keyrings
               curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
               echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu ${ubuntuCodeName} stable" \
                 | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
