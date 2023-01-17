0fce2c5a253d50f21d91eb2d8f6ac229a2700f42
Carl Chen <ccf.developer@gmail.com>
2023-01-17 10:14:03 +0800

    fix: ssh config bugs

--- configs/git/force_ssh_for_github.sh
+++ configs/git/force_ssh_for_github.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "==> [INFO] use ssh instead of https for github in current git repository"
+echo "==> [INFO] use global ssh config for replace https to ssh for github.com"
 
-git config --add url."git@github.com:".insteadOf "https://github.com/"
+git config --global --add url."git@github.com:".insteadOf "https://github.com/"
 
 echo "==> Done."
-echo "  run command 'git config --get url.\"git@github.com:\".insteadOf' to see configuration for current repository."
