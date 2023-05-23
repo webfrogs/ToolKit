49488fe9c0832eab1ecf47b72593500978c5a58e
Carl Chen <ccf.developer@gmail.com>
2023-05-23 10:07:28 +0800

    fix: autojump issue

--- installer/manjaro/autojump_fix.sh
+++ installer/manjaro/autojump_fix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-for autojumplib in $(ls /usr/lib/python3.10/site-packages | grep '^auto'); do
-  sudo ln -s /usr/lib/python3.10/site-packages/${autojumplib} /usr/lib/python3.9/site-packages/${autojumplib}
+for autojumplib in $(ls /usr/lib/python3.11/site-packages | grep '^auto'); do
+  sudo ln -sf /usr/lib/python3.11/site-packages/${autojumplib} /usr/lib/python3.10/site-packages/${autojumplib}
 done
 
 echo "Done."
