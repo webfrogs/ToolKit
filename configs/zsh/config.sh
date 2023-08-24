90e9c4848d58117e804663723d8f5b2a1f4eee0a
Carl Chen <ccf.developer@gmail.com>
2023-08-24 11:58:40 +0800

    fix: remove autojump installation in zsh config script

--- configs/zsh/config.sh
+++ configs/zsh/config.sh
@@ -58,31 +58,3 @@ if test $(basename $SHELL) != "zsh"; then
 fi
 
 echo "==> zsh configuration done."
-
-# install autojump
-echo ""
-echo "==> try to install missing package."
-if test ! -x "$(command -v autojump)"; then
-  case "$(uname -s)" in
-    Darwin)
-      brew install autojump
-      ;;
-    Linux)
-      if test -x "$(command -v pacman)"; then
-        if test -x "$(command -v yay)"; then
-          yay -S autojump --noconfirm
-        else
-          echo "[WARN] yay command is not found. please install 'yay' first before 'autojump' package"
-        fi
-      elif test -x "$(command -v apt-get)"; then
-        sudo apt-get install -y autojump
-      else
-        echo "[WARN] Unsupported package management. Please install 'autojump' package manually."
-      fi
-      ;;
-    *)
-      echo "[WARN] Unsupported OS. Please install 'autojump' package manually."
-      ;;
-  esac
-fi
-
