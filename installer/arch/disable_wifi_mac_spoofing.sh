a4717e7be4040d31dd50e0b6faef3bee1065c04a
Carl Chen <ccf.developer@gmail.com>
2024-09-14 18:31:25 +0800

    feat: [arch] dishable wifi mac spoofing

--- installer/arch/disable_wifi_mac_spoofing.sh
+++ installer/arch/disable_wifi_mac_spoofing.sh
@@ -5,9 +5,9 @@ sudo tee -a /etc/NetworkManager/conf.d/90-disable-wifi-mac-spoofing.conf >/dev/n
 [device-mac-randomization]
 wifi.scan-rand-mac-address=no
 
-#[connection-mac-randomization]
-#ethernet.cloned-mac-address=random
-#wifi.cloned-mac-address=random
+[connection-mac-randomization]
+ethernet.cloned-mac-address=permanent
+wifi.cloned-mac-address=permanent
 EOF
 
 sudo systemctl restart NetworkManager
