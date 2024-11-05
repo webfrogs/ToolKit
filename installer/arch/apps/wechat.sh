19614d65eb68c9923e88333f0c9c345016b32fd6
Carl Chen <ccf.developer@gmail.com>
2024-11-05 16:49:47 +0800

    fix: arch wechat installation script issue

--- installer/arch/apps/wechat.sh
+++ installer/arch/apps/wechat.sh
@@ -3,11 +3,11 @@ set -e
 
 yay -S wechat-uos-qt --noconfirm
 
-sudo tee /usr/local/bin/wechat >/dev/null <<'EOF'
-#!/bin/sh
-set -e 
+# sudo tee /usr/local/bin/wechat >/dev/null <<'EOF'
+# #!/bin/sh
+# set -e 
 
-trashAppUnsafe=1 /usr/bin/wechat-uos-qt
-EOF
+# trashAppUnsafe=1 /usr/bin/wechat-uos-qt
+# EOF
 
-sudo chmod +x /usr/local/bin/wechat
+# sudo chmod +x /usr/local/bin/wechat
