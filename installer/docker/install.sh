15f58e982c2e98aa0274e9da75245d2fb7b88c15
Carl Chen <ccf.developer@gmail.com>
2024-12-16 19:26:20 +0800

    fix: docker install on debian OS

--- installer/docker/install.sh
+++ installer/docker/install.sh
@@ -20,6 +20,24 @@ case "$(uname -s)" in
         fi
 
         if test -x "$(command -v apt-get)"; then
+          if test "$(grep '^ID' /etc/os-release | cut -d= -f2)" == "debian"; then
+            # debian
+            # Add Docker's official GPG key:
+            sudo apt-get update
+            sudo apt-get install ca-certificates curl
+            sudo install -m 0755 -d /etc/apt/keyrings
+            sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+            sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+            # Add the repository to Apt sources:
+            echo \
+              "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+              $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+              sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            sudo apt-get update
+            sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+          else
+            # ubuntu
             sudo apt-get update
             sudo apt-get install -y \
                 apt-transport-https \
@@ -50,6 +68,7 @@ case "$(uname -s)" in
             
             sudo apt-get update
             sudo apt-get install -y docker-ce
+          fi
         elif test -x "$(command -v yum)"; then
             sudo yum install -y yum-utils \
                 device-mapper-persistent-data \
