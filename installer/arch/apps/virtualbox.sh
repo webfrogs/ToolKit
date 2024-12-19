61558ac4c705f3285c232cb10d47620db34a8eb5
Carl Chen <ccf.developer@gmail.com>
2024-12-19 17:39:49 +0800

    fix: virtualbox installation script

--- installer/arch/apps/virtualbox.sh
+++ installer/arch/apps/virtualbox.sh
@@ -7,3 +7,5 @@ linux_kernel=$(mhwd-kernel -li | grep '*' | awk '{print $2}')
 sudo pacman -S virtualbox ${linux_kernel}-virtualbox-host-modules virtualbox-guest-utils
 sudo vboxreload
 sudo pamac install virtualbox-guest-iso
+
+sudo gpasswd -a $USER vboxusers
