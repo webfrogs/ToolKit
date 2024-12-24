3df9e58285e69780ded44001492ced33c78f87ab
Carl Chen <ccf.developer@gmail.com>
2024-12-24 22:23:52 +0800

    feat: add archlinuxcn source from script instead of bootstrap

--- bootstrap/archlinux.sh
+++ bootstrap/archlinux.sh
@@ -34,25 +34,6 @@ fi
 echo "==> start to bootstrap archlinux."
 sudo pacman -Syy
 
-echo "==> add archlinuxcn pacman source."
-if grep -Fxq "[archlinuxcn]" /etc/pacman.conf; then
-	echo "[INFO] archlinux cn already exists in file '/etc/pacman.conf'"
-else
-	echo "[archlinuxcn]" | sudo tee -a /etc/pacman.conf
-	echo "SigLevel = Optional TrustedOnly" | sudo tee -a /etc/pacman.conf
-	echo 'Server = http://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch' | sudo tee -a /etc/pacman.conf
-  sudo pacman -Syy
-	sudo pacman -S archlinuxcn-keyring haveged --noconfirm
-
-  # fix archlinuxcn key can not import
-  # see https://www.archlinuxcn.org/gnupg-2-1-and-the-pacman-keyring/
-  sudo systemctl enable --now haveged
-  sudo rm -rf /etc/pacman.d/gnupg
-  sudo pacman-key --init
-  sudo pacman-key --populate archlinux
-  sudo pacman-key --populate archlinuxcn
-fi
-
 # install necessary packages
 echo "==> install necessary packages."
 mkdir -p ${HOME}/Pictures/screenshots
