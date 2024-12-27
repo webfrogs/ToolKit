285c5e89d321645580a16da28ff8d64e3ebb36fb
Carl Chen <ccf.developer@gmail.com>
2024-12-27 16:21:08 +0800

    feat: add 1password arch install script

--- installer/arch/apps/edge.sh
+++ installer/arch/apps/edge.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-# yay -S microsoft-edge-stable
+yay -S --noconfirm microsoft-edge-stable-bin
 
-cd /tmp
-rm -rf microsoft-edge-stable-bin
-git clone https://aur.archlinux.org/microsoft-edge-stable-bin.git
-cd microsoft-edge-stable-bin
-makepkg -si
-cd ..
-rm -rf microsoft-edge-stable-bin
+# cd /tmp
+# rm -rf microsoft-edge-stable-bin
+# git clone https://aur.archlinux.org/microsoft-edge-stable-bin.git
+# cd microsoft-edge-stable-bin
+# makepkg -si
+# cd ..
+# rm -rf microsoft-edge-stable-bin
