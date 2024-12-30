6a027d310d3eb5e801730e50273d1f8b3af09730
Carl Chen <ccf.developer@gmail.com>
2024-12-30 18:39:15 +0800

    fix: docker qemu config script run failed

--- installer/docker/qemu_config.sh
+++ installer/docker/qemu_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker run --pull --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker run --pull=always --rm --privileged multiarch/qemu-user-static --reset -p yes
 echo "Done."
