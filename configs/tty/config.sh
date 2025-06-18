752836d0454bb6832afc0a9d66f795f17274cb97
Carl Chen <ccf.developer@gmail.com>
2025-06-18 12:08:03 +0800

    feat: change kitty theme

--- configs/tty/config.sh
+++ configs/tty/config.sh
@@ -10,11 +10,11 @@ fi
 mkdir -p $(dirname ${ghostty_config_path})
 ln -sf ${current_folder}/ghostty_config ${ghostty_config_path}
 
-kitty_config_path=$HOME/.config/kitty/kitty.conf
-if test -f ${kitty_config_path}; then
-  rm -f ${kitty_config_path}
+kitty_config_path=$HOME/.config/kitty
+if test -e ${kitty_config_path}; then
+  rm -rf ${kitty_config_path}
 fi
 mkdir -p $(dirname ${kitty_config_path})
-ln -sf ${current_folder}/kitty_config ${kitty_config_path}
+ln -sf ${current_folder}/kitty ${kitty_config_path}
 echo "Done!"
 
