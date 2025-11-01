ea7d62567c39ae190f438d9bcc6e82a6369bd732
Carl Chen <ccf.developer@gmail.com>
2025-11-01 23:05:07 +0800

    feat: add hyprland install script

---
+++ installer/hyprland/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+cd $(dirname $0)
+current_dir=$(pwd)
+
+installNeed="y"
+if test -x "$(command -v hyprland)"; then
+	echo "[INFO] hyprland has been installed."
+	read -p "Need reinstall? [y/n] " installNeed
+fi
+
+if test "${installNeed}" = "y"; then
+  echo "==> install necessary packages."
+	if test -x "$(command -v pacman)"; then
+    sudo pacman -Syy
+    sudo pacman -S --noconfirm \
+      hyprland fuzzel waybar
+  else
+		echo "no supported package manager found."
+		exit 1
+  fi
+fi
+
+rm -rf ${HOME}/.config/hypr
+ln -sf ${current_dir}/config ${HOME}/.config/hypr
+
+# handle monitor conf
+if test -e config/conf/monitor.conf -a ! -L config/conf/monitor.conf; then
+  rm -f config/conf/monitor.conf
+fi
+if test ! -e config/conf/monitor.conf; then 
+  ln -sf ./monitors/default.conf config/conf/monitor.conf
+fi
+
+
+echo "==> All done!"
