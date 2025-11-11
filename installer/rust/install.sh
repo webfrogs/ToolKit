778c2bae8fdad9f81d8df3c8c05d54f37f60dc93
Carl Chen <ccf.developer@gmail.com>
2025-11-11 19:06:57 +0800

    feat: update rust install script

--- installer/rust/install.sh
+++ installer/rust/install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 set -e
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+if test -x "$(command -v pacman)"; then
+  sudo pacman -Syy
+  sudo pacman -S rustup
+  rustup default stable
+else
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
