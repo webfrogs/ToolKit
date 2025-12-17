d54e2c29710b61581ab27865cdd6a782e154731f
Carl Chen <ccf.developer@gmail.com>
2025-12-17 17:51:12 +0800

    feat: set open file window as float on hyprland

--- installer/arch/driver/rtl88x2bu.sh
+++ installer/arch/driver/rtl88x2bu.sh
@@ -10,7 +10,7 @@ sudo pacman -S bc dkms linux-lts-headers
 cd /tmp
 rm -rf rtl88x2bu
 git clone https://github.com/cilynx/rtl88x2bu.git
-cd rtl88x2bu
+# cd rtl88x2bu
 sudo dkms add ./rtl88x2bu
 sudo dkms autoinstall
 dkms status
