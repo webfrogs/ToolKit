47b47629766b2af555de1449000aef4215507fe1
Carl Chen <ccf.developer@gmail.com>
2026-01-26 17:06:33 +0800

    feat: add arch installation script for ai

--- installer/arch/apps/ai.sh
+++ installer/arch/apps/ai.sh
@@ -3,7 +3,12 @@ set -e
 
 cd $(dirname $0)
 
+# opencode
 paru -S opencode-bin
 bunx oh-my-opencode install
+
+# gemini-cli
+npm install -g @google/gemini-cli
+gemini extensions install https://github.com/github/github-mcp-server
 # ../bin/aur_install.sh 
 
