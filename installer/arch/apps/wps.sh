f799a7a7d8f52c2f59afca46ff2127e1bb63650c
Carl Chen <ccf.developer@gmail.com>
2026-01-29 15:50:19 +0800

    feat: wps installation script update

--- installer/arch/apps/wps.sh
+++ installer/arch/apps/wps.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 
+# wiki: https://wiki.archlinuxcn.org/wiki/WPS_Office
 cd $(dirname $0)
-../bin/aur_install.sh wps-office wps-office-mui-zh-cn libtiff5
+../bin/aur_install.sh wps-office-cn wps-office-fonts wps-office-mui-zh-cn libtiff5
