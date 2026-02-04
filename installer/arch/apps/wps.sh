f06e4cbb52700865118c266d960e7b7cbf4ece7c
Carl Chen <ccf.developer@gmail.com>
2026-02-04 18:32:01 +0800

    fix: wps open word file in file manager issue

--- installer/arch/apps/wps.sh
+++ installer/arch/apps/wps.sh
@@ -4,3 +4,8 @@ set -e
 # wiki: https://wiki.archlinuxcn.org/wiki/WPS_Office
 cd $(dirname $0)
 ../bin/aur_install.sh wps-office-cn wps-office-fonts wps-office-mui-zh-cn libtiff5
+
+if test -f "~/.config/Kingsoft/Office.conf"; then
+  sed -i '/^wpsoffice\\Application%20Settings\\AppComponentMode/d' ~/.config/Kingsoft/Office.conf
+  sed -i '/^\[6\.0\]/a\wpsoffice\\Application%20Settings\\AppComponentMode=prome_independ' ~/.config/Kingsoft/Office.conf
+fi
