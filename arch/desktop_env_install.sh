04dd01b48f2b11b4e4e6da21057f19fbb5fcd258
Carl Chen <ccf.developer@gmail.com>
2026-03-12 11:04:51 +0800

    feat: install rime-ice input method for archlinux desktop

--- arch/desktop_env_install.sh
+++ arch/desktop_env_install.sh
@@ -15,8 +15,15 @@ sudo pacman -S --noconfirm \
 
 
 # install chinese input method
-# sudo pacman -S --noconfirm fcitx-im fcitx-configtool fcitx-sunpinyin
-sudo pacman -S --noconfirm fcitx5-im fcitx5-chinese-addons
+sudo pacman -S --noconfirm fcitx5-im fcitx5-chinese-addons fcitx5-rime
+./bin/aur_install.sh rime-ice-git || true
+mkdir -p $HOME/.local/share/fcitx5/rime/
+cat <<'EOF' | tee $HOME/.local/share/fcitx5/rime/default.custom.yaml
+patch:
+  "menu/page_size": 10
+  # 这里的 rime_ice_suggestion 为雾凇方案的默认预设
+  __include: rime_ice_suggestion:/
+EOF
 
 
 sudo pacman -S --noconfirm noto-fonts-emoji # fix emoji
