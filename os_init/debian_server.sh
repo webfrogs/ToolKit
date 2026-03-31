da447b6492dd946e3153298e7d41c728fb870818
Carl Chen <ccf.developer@gmail.com>
2026-03-31 16:49:52 +0800

    feat: add no-cn-mirror option to debian server bootstrap script

--- os_init/debian_server.sh
+++ os_init/debian_server.sh
@@ -5,6 +5,31 @@ cd $(dirname $0)
 cd ..
 RootPath=$(pwd)
 
+if test $# -gt 0; then
+  ARGS=$(getopt -o '' --long no-cn-mirror -n "$(basename "$0")" -- "$@")
+  if test $? != 0; then
+    echo "ERROR! $(basename "$0") options error"
+    exit 1
+  fi
+  eval set -- "${ARGS}"
+  while true; do
+    case "$1" in
+      --no-cn-mirror)
+        OPT_NO_CN_MIRROR="1"
+        shift
+        ;;
+      --)
+        shift
+        break
+        ;;
+      *)
+        echo "ERROR! command options handle failed"
+        exit 1
+        ;;
+    esac
+  done
+fi
+
 if test "$(uname -s)" != "Linux"; then
   echo "[ERROR] Current OS is not Linux"
   exit 1
@@ -22,7 +47,8 @@ sudo apt install -y \
 
 ./configs/git/config.sh
 ./installer/fzf/install.sh
-./installer/docker/install.sh
-
-
-
+if test "${OPT_NO_CN_MIRROR}" = "1"; then
+  ./installer/docker/install.sh
+else
+  ./installer/docker/install.sh --cn-mirror
+fi
