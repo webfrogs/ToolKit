ae84195c6bfdfde884c93109240087a27159b99f
Carl Chen <ccf.developer@gmail.com>
2026-06-22 12:18:38 +0800

    chore(zsh): update ai_commit function to use opencode
    - change ai_commit to run opencode instead of agy

--- configs/zsh/conf/ai.zsh
+++ configs/zsh/conf/ai.zsh
@@ -1,3 +1,4 @@
 ai_commit() {
-  agy --dangerously-skip-permissions --model "Gemini 3.5 Flash (Medium)" -p "current folder is workspace, make git commit"
+  # agy --dangerously-skip-permissions --model "Gemini 3.5 Flash (Medium)" -p "current folder is workspace, make git commit"
+  opencode run --dangerously-skip-permissions -m 'deepseek/deepseek-v4-flash' 'make git commit'
 }
